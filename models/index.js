e4bf2c7bd1f19c4bf55cf345b2312febafe882cb
MiguelAngelZ1 <miguelanyelo0011@gmail.com>
2025-06-11 11:06:17 -0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,15 @@
-const Usuario = require('./Usuario');
-const GastoFijo = require('./GastoFijo');
+const { Usuario } = require('./Usuario');
+const { GastoFijo } = require('./GastoFijo');
 
-// Relaciones
-Usuario.hasMany(GastoFijo, { foreignKey: 'usuario_id' });
-GastoFijo.belongsTo(Usuario, { foreignKey: 'usuario_id' });
+// Configura las relaciones
+function setupRelations() {
+  Usuario.hasMany(GastoFijo, { foreignKey: 'usuario_id' });
+  GastoFijo.belongsTo(Usuario, { foreignKey: 'usuario_id' });
+}
 
 module.exports = {
   Usuario,
-  GastoFijo
+  GastoFijo,
+  setupRelations,
+  sequelize: require('../config/database')
 };
